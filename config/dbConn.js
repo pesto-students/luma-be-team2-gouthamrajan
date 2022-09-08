09871381ea400abd6541cd0556bee0228d0b391b
KetanGadwale <ketanvgadwale@gmail.com>
2022-09-08 11:28:59 +0530

    fix: database url fixed

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      'mongodb+srv://User:ejFb8vxlTInF9dPa@cluster0.6v7nhfa.mongodb.net/?retryWrites=true&w=majority',
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      }
-    );
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
   } catch (err) {
     console.error(err);
   }
