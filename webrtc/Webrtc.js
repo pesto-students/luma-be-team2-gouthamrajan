a2306d3e38330b7f05e1d703c38a93a2689447c5
advaitkr <advaitkr91@gmail.com>
2022-09-17 03:26:48 +0530

    webrtc updates

--- webrtc/Webrtc.js
+++ webrtc/Webrtc.js
@@ -2,7 +2,7 @@ const Webrtc = require('./Model');
 const router = require('express').Router();
 const axios = require('axios').default;
 router.get("/",async(req,res)=>{
-    axios.get("http://localhost:3030/meeting")
+    axios.get("https://luma-webrtc.herokuapp.com/meeting")
     .then(re=>{
         console.log(re)
         res.status(200).send(re.data)
